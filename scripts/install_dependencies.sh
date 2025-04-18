b8486b5e453da9ca8d5f4fddb7c974e3e392a56b
Martin Grader <martingrader@gmail.com>
2025-04-18 13:40:20 +0200

    deactivate tmux pane change shortcuts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,15 +6,15 @@ dotfiles_dir=~/dotfiles
 log_file=$dotfiles_dir/link_install.txt
 
 echo "installing vim"
-sudo apt install vim >> $log_file 2>&1
+sudo apt --assume-yes install vim >> $log_file 2>&1
 echo "installing tmux"
-sudo apt-get install tmux >> $log_file 2>&1
+sudo apt-get --assume-yes install tmux >> $log_file 2>&1
 echo "installing fzf"
-sudo apt install fzf >> $log_file 2>&1
+sudo apt --assume-yes install fzf >> $log_file 2>&1
 echo "installing curl"
-sudo apt install curl >> $log_file 2>&1
+sudo apt --assume-yes install curl >> $log_file 2>&1
 echo "installing zsh"
-sudo apt install zsh >> $log_file 2>&1
+sudo apt --assume-yes install zsh >> $log_file 2>&1
 
 #echo "making zsh default (remember to restart)"
 #chsh -s $(which zsh) >> $log_file 2>&1
@@ -34,17 +34,17 @@ echo "installing bash language server"
 sudo snap install bash-language-server --classic >> $log_file 2>&1
 
 echo "installing pip"
-sudo apt install pip >> $log_file 2>&1
+sudo apt --assume-yes install pip >> $log_file 2>&1
 echo "installing pipx"
-sudo apt install pipx >> $log_file 2>&1
+sudo apt --assume-yes install pipx >> $log_file 2>&1
 pipx ensurepath >> $log_file 2>&1
 echo "installing poetry"
 pipx install poetry >> $log_file 2>&1
 poetry config virtualenvs.in-project true >> $log_file 2>&1
 echo "installing nodejs"
-sudo apt install nodejs >> $log_file 2>&1
+sudo apt --assume-yes install nodejs >> $log_file 2>&1
 echo "installing npm"
-sudo apt install npm >> $log_file 2>&1
+sudo apt --assume-yes install npm >> $log_file 2>&1
 
 #:CocInstall coc-json
 #:CocInstall @yaegassy/coc-ruff
--- zshrc
+++ zshrc
@@ -109,7 +109,7 @@ export EDITOR='vim'
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
-alias cdc="cd /media/Daten/Code/"
+alias cdc="cd /media/shared/Code/"
 
 # Turn off all beeps
 unsetopt BEEP
